68fdec5bef73873cefc06b5a773ffd7fa3b910fd
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-23 15:55:51 -0500

    number generator

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ spin.addEventListener('click', spinToWin)
 
 function spinToWin(){
  const winningNumber =  Math.floor(Math.random(0) * 36);
- winnerDisplay.innerHTML = winningNumber;
  console.log(winningNumber)
+ if(winningNumber% 2 === 0 && winningNumber != 0){
+  winnerDisplay.innerHTML = `${winningNumber} Black`;
+
+ }
+
+ else if(winningNumber !== 0){
+  winnerDisplay.innerHTML = `${winningNumber} Red`;
+ }
+
+ else{
+  winnerDisplay.innerHTML = winningNumber;
+ }
+
+
+
+
+
+
 }
